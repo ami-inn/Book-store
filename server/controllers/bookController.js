d5c62fb6e0efc08888a73406982a978e80158758
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-10-30 16:14:58 +0530

    edit book controller added

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,38 +1,66 @@
-
 import { catchAsyncError } from "../utils/catchAsyncError";
 import BookModel from "../model/book.model";
 import mongoose from "mongoose";
-import path from 'path'
+import path from "path";
 import ErrorHandler from "../utils/ErrorHandler";
-import cloudinary from 'cloudinary'
+import cloudinary from "cloudinary";
 import { createBook } from "../services/book.service";
 
+export const uploadCourse = catchAsyncError(async (req, res, next) => {
+  try {
+    const data = req.body;
 
-export const uploadCourse = catchAsyncError(async(req,res,next)=>{
-    try{
+    const thumbnail = data.thumbnail;
 
-        const data = req.body
+    if (thumbnail) {
+      const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+        folder: "books",
+      });
 
-        const thumbnail = data.thumbnail
-
-        if(thumbnail){
-            const myCloud  = await cloudinary.v2.uploader.upload(thumbnail,{
-                folder:"books"
-            })
-
-            data.thumbnail = {
-                public_id: myCloud.public_id,
-                url:myCloud.secure_url
-            }
-        }
-
-        createBook(data,res,next)
-
+      data.thumbnail = {
+        public_id: myCloud.public_id,
+        url: myCloud.secure_url,
+      };
     }
 
-    catch(error){
-        return next(new ErrorHandler(error.message,500))
-    }
-})
+    createBook(data, res, next);
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
 
-export const 
+export const editBook = catchAsyncError(async (req, res, next) => {
+  try {
+    const data = req.body;
+
+    const thumbnail = data.thumbnail;
+
+    if (thumbnail) {
+      await cloudinary.v2.uploader.destroy(thumbnail.public_id);
+
+      const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+        folder: "books",
+      });
+
+      data.thumbnail = {
+        public_id: myCloud.public_id,
+        url: myCloud.secure_url,
+      };
+    }
+      const bookId = req.params.id;
+
+      const book = await BookModel.findByIdAndUpdate(
+        bookId,
+        { $set: data },
+        { new: true }
+      );
+    
+      res.status(201).json({
+        success: true,
+        book
+      })
+
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
